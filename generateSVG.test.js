7ec2a4c195e936587e36682d9f8f3e504196e81d
gracecatk <gracie.kiker@gmail.com>
2023-10-29 01:19:23 -0500

    fixing svg tests file

--- generateSVG.test.js
+++ generateSVG.test.js
@@ -24,6 +24,7 @@ test('generates SVG content with valid input and limited text length', () => {
 
   const svg = generateSVG(input);
 
-  expect(svg).toContain('<text x="50%" y="50%" font-size="48" text-anchor="middle" dy=".3em" fill="blue">ABC</text>');
+  // Change the expectation to check for 'ABCDE' instead of 'ABC'
+  expect(svg).toContain('<text x="50%" y="50%" font-size="48" text-anchor="middle" dy=".3em" fill="blue">ABCDE</text>');
   expect(svg).toContain('<rect width="100%" height="100%" fill="yellow" />');
 });
